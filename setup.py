a4474a411516bf3bd8eef7a26994d72707999ce8
Noam <noamba@gmail.com>
2022-04-30 22:49:55 +0100

    update setup

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 
 # Learn more: https://github.com/kennethreitz/setup.py
 
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 with open("README.md") as f:
     readme = f.read()
@@ -20,7 +19,8 @@ setup(
     author_email="noamba@gmail.com",
     url="https://github.com/noamba/minimum",
     license=license,
-    py_modules=["minimum", "tests"],
+    packages=find_packages(),
+    # py_modules=["minimum", "tests"],
     install_requires=[
         "pytest",
     ],
