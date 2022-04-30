ea6f4d5e4d0c511d0371668011e59647a2c0f782
Noam <noamba@gmail.com>
2022-04-30 21:32:10 +0100

    remove setup.cfg, replace with setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="minimum",
+    version="0.1.0",
+    description="Find the minimal element in a specific type of array",
+    long_description=readme,
+    author="Noam Bamberger",
+    author_email="noamba@gmail.com",
+    url="https://github.com/noamba/minimum",
+    license=license,
+    packages=find_packages(),
+    install_requires=[
+        "pytest",
+    ],
+)
