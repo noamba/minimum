116527622c99be6279d7f4f28a910b8447942794
Noam <noamba@gmail.com>
2022-05-01 14:22:12 +0100

    move consts to consts

---
+++ minimum/constants.py
@@ -0,0 +1,3 @@
+LESS_THAN_3_ELEMENTS = "Array must be at least 3 elements long"
+NOT_ITERABLE = "is not iterable"
+DOES_NOT_IMPLEMENT_LESS_THAN = "does not implement '<'"
--- minimum/minimum_cls.py
+++ minimum/minimum_cls.py
@@ -1,6 +1,8 @@
-LESS_THAN_3_ELEMENTS = "Array must be at least 3 elements long"
-NOT_ITERABLE = "is not iterable"
-DOES_NOT_IMPLEMENT_LESS_THAN = "does not implement '<'"
+from minimum.constants import (
+    LESS_THAN_3_ELEMENTS,
+    NOT_ITERABLE,
+    DOES_NOT_IMPLEMENT_LESS_THAN,
+)
 
 
 class Minimum:
